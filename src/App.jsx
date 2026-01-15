611dfc79d1ad4f3fc151a2d4d0d8e91c27770bea
achyuth810 <achyuthreddyrodda@gmail.com>
2026-01-15 00:34:05 -0600

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Contact from "./pages/Contact";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-black">
+    <div className="min-h-screen bg-slate-50">
       <Navbar />
 
       <Routes>
--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -3,7 +3,7 @@ import Footer from "./Footer";
 
 export default function AppLayout({ children }) {
   return (
-    <div className="bg-white min-h-screen">
+    <div className="min-h-screen bg-slate-50">
       <Navbar />
       {/* Push content below fixed navbar */}
       <main className="pt-24">{children}</main>
